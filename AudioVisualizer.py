c48f776016b8d289b9df59ab7c3c206e78d829c4
Sam DeGroot <samueldegroot@yahoo.com>
2020-11-08 23:42:30 -0600

    First fully functional prototype, mono audio support only and single color display

--- AudioVisualizer.py
+++ AudioVisualizer.py
@@ -2,50 +2,18 @@
 #Audio Visualizer
 #Blake DeGroot and Brandon Namphong
 
-import RPi.GPIO as GPIO
 import numpy as np
+import scipy
 import pyaudio
+from PIL import Image
+from PIL import ImageDraw
 import time
-import matplotlib.pyplot as plt
- 
-delay = 0.000001
+from rgbmatrix import RGBMatrix, RGBMatrixOptions
 
-#LED Panel Setup
-"""
-GPIO.setwarnings(False)
-GPIO.setmode(GPIO.BCM)
-red1_pin = 17
-green1_pin = 18
-blue1_pin = 22
-red2_pin = 23
-green2_pin = 24
-blue2_pin = 25
-clock_pin = 3
-a_pin = 7
-b_pin = 8
-c_pin = 9
-latch_pin = 4
-oe_pin = 2
- 
-GPIO.setup(red1_pin, GPIO.OUT)
-GPIO.setup(green1_pin, GPIO.OUT)
-GPIO.setup(blue1_pin, GPIO.OUT)
-GPIO.setup(red2_pin, GPIO.OUT)
-GPIO.setup(green2_pin, GPIO.OUT)
-GPIO.setup(blue2_pin, GPIO.OUT)
-GPIO.setup(clock_pin, GPIO.OUT)
-GPIO.setup(a_pin, GPIO.OUT)
-GPIO.setup(b_pin, GPIO.OUT)
-GPIO.setup(c_pin, GPIO.OUT)
-GPIO.setup(latch_pin, GPIO.OUT)
-GPIO.setup(oe_pin, GPIO.OUT)
-"""
 
 SAMPLE_RATE = 44100 #sampling rate of the adc to be used
-CHUNK = 2**13 # number of data points to read at a time
-#FPS = 30 #speed at which the display will update, frames per second
-xres = 32 #number of leds in the x plane, rows
-yres = 16 #number of leds in the y plane, columns
+CHUNK = 2**10 # number of data points to read at a time
+VOL_SCALE = 1000 #minimum unit of frequency amplitudes (placeholder value)
 
 #32 bins for frequency grouping for the 32 led columns, 0 at end for last comparison
 freqbins = [20. ,    24.8,    30.8,    38.2,    47.4,    58.9,    73. ,
@@ -53,33 +21,65 @@ freqbins = [20. ,    24.8,    30.8,    38.2,    47.4,    58.9,    73. ,
          410.7,   509.7,   632.5,   784.8,   973.9,  1208.6,  1499.8,
         1861.1,  2309.6,  2866. ,  3556.6,  4413.5,  5476.8,  6796.4,
         8433.9, 10466. , 12987.6, 16116.8, 20000., 0.]
+ 
+# Configuration for the matrix
+options = RGBMatrixOptions()
+options.rows = 16
+options.hardware_mapping = 'audio-visualizer'
 
-
+matrix = RGBMatrix(options = options)
+image = Image.new("RGB", (32, 16))  # Create image object to store lines in
+draw = ImageDraw.Draw(image)  # Draw object to store lines
+ 
 p=pyaudio.PyAudio() # start the PyAudio class
 stream=p.open(format=pyaudio.paInt16,channels=1,input_device_index=0,rate=SAMPLE_RATE,input=True,
               frames_per_buffer=CHUNK) #uses default input device
 
 #TODO: add support for multiple channels (stereo)
 
-# infinite loop that reads data from adc and performs fft, then sends instructions to led
+#set up frequency bins
+freqdata = np.fft.fftfreq(CHUNK,1.0/SAMPLE_RATE) # find frequency bins of data
+freqdata = freqdata[:int(len(freqdata)/2)] # keep only first half
+freq_indices = [0]*32
+for i in range(32):
+    freq_indices[i] = np.where(np.logical_and((freqdata>freqbins[i]),(freqdata<freqbins[i+1])))[0] # select indices where the freq falls in the selected bin
+    #print(len(freq_indices[i]))
+
+prev_volume = [0]*32 #used to slowly reduces spikes for better looking graphics
+
+# loop that reads data from adc and performs fft, then sends instructions to led, ctrl+C to exit
 try:
     while True:
-        data = np.frombuffer(stream.read(CHUNK),dtype=np.int16)
+        data = np.frombuffer(stream.read(CHUNK),dtype=np.int16) # read data from RCA input
         data = data * np.hamming(len(data)) # smooth the FFT by windowing data
-        fft = abs(np.fft.fft(data).real)
-        fft = fft[:int(len(fft)/2)] # keep only first half
-        freq = np.fft.fftfreq(CHUNK,1.0/SAMPLE_RATE)
-        freq = freq[:int(len(freq)/2)] # keep only first half
-        for i in range(0,xres):
-            #select indices where the freq falls in the selected bin
-            freq_indices = np.where(np.logical_and((freq>freqbins[i]),(freq<freqbins[i+1])))[0]
-            amplitude = np.sum(fft[freq_indices])
-            #TODO: add led instructions
+        fftdata = abs(scipy.fft(data)) # perform FFT on data
+        fftdata = fftdata[:int(len(fftdata)/2)] # keep only first half
+        draw.rectangle((0, 0, 31, 15), fill=(0,0,0))
+        for i in range(32):
+            amplitude = np.sum(fftdata[freq_indices[i]])# sum amplitude of all elements of fftdata in current frequency range
+            amplitude = amplitude / (1+0.001*i) # scale down amplitude by index to counter oversampling of higher frequencies
+            volume = int(np.floor(amplitude / VOL_SCALE)) # scale amplitude to number of led rows to light up
+            if volume > 16: # if volume is over max, set to max
+                volume = 16
+            if volume < prev_volume[i]: # if volume is lower than prev_volume, slowly lower bar rather than instantly
+                volume = int(prev_volume[i]*0.99)
+            xpos = 31-i
+            draw.line((xpos, 0, xpos, volume), fill=(0, 0, 255)) # draw amplitude line
+            if volume > 4: #if high volume, add curve around bar
+                draw.line((xpos, 0, xpos, volume/2), width=5, fill=(0, 0, 255))
+                draw.arc((xpos+1, 0, xpos+7, volume*2 - volume/3), 180, 270, fill=(0, 0, 255))
+                draw.arc((xpos-7, 0, xpos-1, volume*2 - volume/5), 270, 0, fill=(0, 0, 255))
+            prev_volume[i] = volume #store volume in previous volume array
+        matrix.Clear() #erase the image currently on matrix
+        matrix.SetImage(image, 0, 0) #add new image to matrix
             
 except KeyboardInterrupt:
+    matrix.Clear()
     print('Program Exiting')
-
-# close the stream gracefully
-stream.stop_stream()
-stream.close()
-p.terminate()
+    #stop threads
+    inputThread.join()
+    # close the stream gracefully
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
+    
